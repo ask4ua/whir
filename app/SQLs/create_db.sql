9337bb317ca3d886fcfbd3ac0786074a4a8e7f78
Volkov Volodymyr <vovo@ask4ua.com>
2019-04-26 12:37:09 +0300

    SQL creating commands for sure;

--- app/SQLs/create_db.sql
+++ app/SQLs/create_db.sql
@@ -1,40 +1,43 @@
-ALTER DATABASE
+create database
     whir
-    CHARACTER SET = utf8mb4
-    COLLATE = utf8mb4_unicode_ci;
+    character set = utf8mb4
+    collate = utf8mb4_unicode_ci;
 
-CREATE TABLE `AUTHORS` (
-    `AUTHOR_ID` VARCHAR(64) NOT NULL,
-	`AUTHOR_NAME` VARCHAR(255) DEFAULT NULL,
-	PRIMARY KEY (`AUTHOR_ID`)
-) ENGINE = INNODB DEFAULT CHARSET = UTF8MB4;
+use whir;
 
-CREATE TABLE `MESSAGES` (
-    `MESSAGE_ID` CHAR(64) NOT NULL,
-	`TEXT` VARCHAR(4000) DEFAULT NULL,
-	`AUTHOR_ID` CHAR(64) DEFAULT NULL,
-	`SOURCE_ID` CHAR(64) DEFAULT NULL,
-	`CREATION_DATE` DATETIME DEFAULT NULL,
-	`FILENAME` VARCHAR(255) DEFAULT NULL,
-	PRIMARY KEY (`MESSAGE_ID`)
-) ENGINE = INNODB DEFAULT CHARSET = UTF8MB4;
+create table `authors` (
+    `author_id` varchar(64) not null,
+   `author_name` varchar(255) default null,
+   primary key (`author_id`)
+) engine = innodb default charset = utf8mb4;
 
-CREATE TABLE `SOURCES` (
-    `SOURCE_ID` VARCHAR(64) NOT NULL,
-	`SOURCE_NAME` VARCHAR(255) DEFAULT NULL,
-	PRIMARY KEY (`SOURCE_ID`)
-) ENGINE = INNODB DEFAULT CHARSET = UTF8MB4;
+create table `messages` (
+    `message_id` char(64) not null,
+   `text` varchar(4000) default null,
+   `author_id` char(64) default null,
+   `source_id` char(64) default null,
+   `creation_date` datetime default null,
+   `filename` varchar(255) default null,
+   primary key (`message_id`)
+) engine = innodb default charset = utf8mb4;
 
-CREATE TABLE `WORDS` (
-    `WORD_ID` CHAR(64) NOT NULL,
-	`TEXT` VARCHAR(4000) DEFAULT NULL,
-	`CREATION_DATE` DATETIME DEFAULT NULL,
-	PRIMARY KEY (`WORD_ID`)
-) ENGINE = INNODB DEFAULT CHARSET = UTF8MB4;
+create table `sources` (
+    `source_id` varchar(64) not null,
+   `source_name` varchar(255) default null,
+   primary key (`source_id`)
+) engine = innodb default charset = utf8mb4;
 
-CREATE TABLE `WORDSINWORD` (
-    `MAINWORD_ID` CHAR(64) NOT NULL,
-	`SUBWORD_ID` CHAR(64) NOT NULL,
-	`COUNT` INT(11) NOT NULL,
-	PRIMARY KEY (`MAINWORD_ID`, `SUBWORD_ID`)
-) ENGINE = INNODB DEFAULT CHARSET = UTF8MB4;
+create table `words` (
+    `word_id` char(64) not null,
+   `text` varchar(4000) default null,
+   `creation_date` datetime default null,
+   primary key (`word_id`)
+) engine = innodb default charset = utf8mb4;
+
+create table `wordsinword` (
+    `mainword_id` char(64) not null,
+   `subword_id` char(64) not null,
+   `count` int(11) not null,
+   primary key (`mainword_id`, `subword_id`)
+) engine = innodb default charset = utf8mb4;
+
