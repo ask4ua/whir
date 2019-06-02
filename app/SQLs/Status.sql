cf3860ca1812cb93036ed26a5bd56eb704a78c84
Volkov Volodymyr <vovo@ask4ua.com>
2019-06-02 10:46:57 +0300

    whir-stack scaled for decomposer to 4

--- app/SQLs/Status.sql
+++ app/SQLs/Status.sql
@@ -1,7 +1,7 @@
--- update messages set inprogress_flag=False;
--- delete from words where words.word_id in (select messages.message_id from messages);
+--update messages set inprogress_flag=False;
+--delete from words where words.word_id in (select messages.message_id from messages);
 
---select count(*) "Messages Total "from messages;
+select count(*) "Messages Total "from messages;
 
 select count(*) "Message Not yet" from messages left join words on words.word_id=messages.message_id where words.word_id is Null;
 
