80b41efa33303b43a23758569e5ed8ee2f68e335
Volkov Volodymyr <vovo@ask4ua.com>
2019-06-07 12:12:14 +0300

    ci scripts fix

--- docker/usefulscripts.sh
+++ docker/usefulscripts.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-docker secret rm whir_db_password >> /dev/null 2>&1 || echo "secret wasn`t exists" 
-docker secret rm root_db_password >> /dev/null 2>&1 || echo "secret wasn`t exists" 
+docker secret rm whir_db_password >> /dev/null 2>&1 || echo "secret wasn't exists"
+docker secret rm root_db_password >> /dev/null 2>&1 || echo "secret wasn't exists"
 
 echo password | docker secret create whir_db_password -
 echo rootp@sswd | docker secret create root_db_password -
