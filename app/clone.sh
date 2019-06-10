a2022f4e1dce6692391763431dd48f3dedbfbfde
Volkov Volodymyr <vovo@ask4ua.com>
2019-06-11 01:45:39 +0300

    not cloned data

--- app/clone.sh
+++ app/clone.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 cd /data
-/usr/bin/git clone http://10.5.33.229:1180/root/whir-testdata.git ./ || git pull whir-testdata/
+/usr/bin/git clone http://10.5.33.229:1180/root/whir-testdata.git ./ || git pull
