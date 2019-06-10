2c7b8f56e6e89ab31769fc6dda2fd590a42e5ff5
Volkov Volodymyr <vovo@ask4ua.com>
2019-06-11 01:27:17 +0300

    not cloned data

--- app/clone.sh
+++ app/clone.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-/usr/bin/git clone --single-branch --branch master https://gogs.ask4ua.com/root/whir-testdata.git /data/
+/usr/bin/git clone https://gogs.ask4ua.com/root/whir-testdata.git /data/
