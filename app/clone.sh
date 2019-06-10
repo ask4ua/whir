b72481824b8d5be13c7a97ddc444f69956d9f9b1
Volkov Volodymyr <vovo@ask4ua.com>
2019-06-11 01:04:19 +0300

    save for sure

--- app/clone.sh
+++ app/clone.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-/usr/bin/git clone --single-branch --branch selftest http://10.5.33.229:1180/root/whir-data /data/
+/usr/bin/git clone --single-branch --branch master https://gogs.ask4ua.com/root/whir-testdata.git /data/
