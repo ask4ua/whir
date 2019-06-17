ab79248a331a46e7a892e2db0176ae9f797a8da3
Volkov Volodymyr <vovo@ask4ua.com>
2019-06-17 18:02:47 +0300

    data git url changed to gogs

--- app/clone.sh
+++ app/clone.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 cd /data
-/usr/bin/git clone http://10.5.33.229:1180/root/whir-testdata.git ./ || git pull
+/usr/bin/git clone https://gogs.ask4ua.com/root/whir-testdata ./ || git pull
