f516ef831779825819231059288fb912a881ae91
Volkov Volodymyr <vovo@ask4ua.com>
2019-06-21 17:19:54 +0300

    save wrhir

--- app/cycle.sh
+++ app/cycle.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-cd /data
-/usr/bin/git clone https://gogs.ask4ua.com/root/whir-testdata.git ./ || /usr/bin/git pull
-cd /app
-python3 parser.py
-python3 decomposer.py
+while true
+do
+    cd /data
+    git stash >> /dev/null 2>&1
+    /usr/bin/git clone https://gogs.ask4ua.com/root/whir-testdata.git ./ || /usr/bin/git pull
+    cd /app
+    python3 parser.py
+    python3 decomposer.py
+done
