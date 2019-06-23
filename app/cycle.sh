f6caf547e8bcc75d823f1e0af72bab304219948a
Volkov Volodymyr <vovo@ask4ua.com>
2019-06-23 20:47:24 +0300

    save

--- app/cycle.sh
+++ app/cycle.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd /data
-/usr/bin/git clone https://gogs.ask4ua.com/root/whir-data.git --branch=eng-books ./ || /usr/bin/git pull
+/usr/bin/git clone https://gogs.ask4ua.com/root/whir-testdata.git ./ || /usr/bin/git pull
+#/usr/bin/git clone https://gogs.ask4ua.com/root/whir-data.git --branch=eng-books ./ || /usr/bin/git pull
 cd /app
 python3 parser.py
 
